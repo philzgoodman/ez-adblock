df1ec58d34f6b64c7d874c8fc92f74c632455d68
philzgoodman <philzgoodmantv@gmail.com>
2022-10-28 12:46:54 -0700

    fully working extension

--- my-extension/config/webpack.config.js
+++ my-extension/config/webpack.config.js
@@ -9,10 +9,9 @@ const PATHS = require('./paths');
 const config = (env, argv) =>
   merge(common, {
     entry: {
-      popup: PATHS.src + '/popup.js',
       contentScript: PATHS.src + '/contentScript.js',
-      background: PATHS.src + '/background.js',
-      css:PATHS.src + '/popup.css'    },
+      background: PATHS.src + '/background.js'
+      },
     devtool: argv.mode === 'production' ? false : 'source-map',
   });
 
--- my-extension/src/contentScript.js
+++ my-extension/src/contentScript.js
@@ -1,56 +1,89 @@
 'use strict';
-import './popup.css';
-
-
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
-  }
-
-);
-
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
-});
 document.addEventListener('load', overrideCSS);
 document.addEventListener('DOM', overrideCSS);
 overrideCSS();
-
-
 function overrideCSS() {
-
-    chrome.scripting.insertCSS({
-      target: { tabId: activeTab.id },
-      files: ["popup.css"],
-      allFrames: true
-    });
-  chrome.tabs.insertCSS({
-    target: { tabId: activeTab.id },
-    files: ["popup.css"]
-  });
-
+  const style = document.createElement('style');
+  style.innerHTML = `
+@media only screen and (max-width: 2500px) {
+  .ha__ribbon,
+  .js_header,
+  #header-wrap,
+  .media__video--thumbnail-wrapper,
+  .advert_container,
+  .jppanel,
+  .adContainer,
+  .GoogleActiveViewElement,
+  div[class^='pogo'],
+  div[class*=' pogo'],
+  div[id^='pogo'],
+  div[id*=' pogo'],
+  div[class^='google'],
+  div[class*=' google'],
+  div[id^='google'],
+  div[id*=' google'],
+  div[class^='goog'],
+  div[class*=' goog'],
+  div[id^='goog'],
+  div[id*=' goog'],
+  div[class^='videoAdUi'],
+  div[class*=' videoAdUi'],
+  div[id^='videoAdUi'],
+  div[id*=' videoAdUi'],
+  div[class^='trueview'],
+  div[class*=' trueview'],
+  div[id^='trueview'],
+  div[id*=' trueview'],
+  div[class^='Sekindo'],
+  div[class*=' Sekindo'],
+  div[id^='Sekindo'],
+  div[id*=' Sekindo'],
+  .videoAdUi,
+  .ima-sdk-frame,
+  .adBanner,
+  .adchoices-container,
+  .adContainerDiv,
+  #adContainerDiv,
+  #imaSlotContainer,
+  #videoContainerDiv,
+  div[class^='primis'],
+  div[class*=' primis'],
+  div[id^='primis'],
+  div[id*=' primis'],
+  .theiaStickySidebar,
+  div[class^='AdBundle'],
+  div[class*=' AdBundle'],
+  div[id^='AdBundle'],
+  div[id*=' AdBundle'],
+  div[class^='unit'],
+  div[class*=' unit'],
+  div[id^='unit'],
+  div[id*=' unit'],
+  .dom_annotate_ad_video_ad,
+  .dom_annotate_ad_promoted_answer,
+  #unit-container,
+  .OUTBRAIN,
+  div[class^='ob'],
+  div[class*=' ob'],
+  div[id^='ob'],
+  div[id*=' ob'],
+  .ob-widget,
+  #stickyadcontainer
+  {
+    margin:0!important;
+    display:none!important;
+    visibility:hidden!important;
+    width:0!important;
+    height:0!important;
+    padding:0!important;
+    opacity:0!important;
+    filter:opacity(0)!important;
+    background:transparent!important;
+  }
+}
+    `;
+  document.head.appendChild(style);
   var link = document.createElement("link");
-  link.href = chrome.extension.getURL("popup.css");
   link.type = "text/css";
   link.rel = "stylesheet";
   document.getElementsByTagName("head")[0].appendChild(link);
